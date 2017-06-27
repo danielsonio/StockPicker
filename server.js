3432aba135a0f1229983505a86ebd748e23633c7
Daniel Walden <Ariel@Daniels-MacBook-Pro.local>
2017-06-26 21:22:23 -0500

    added login functionality

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
+// Requiring necessary npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
+// Setting up port and requiring models for syncing
+var PORT = process.env.PORT || 3000;
+var db = require("./models");
 
-// Sets up the Express App
-// =============================================================
+// Creating express app and configuring middleware needed for authentication
 var app = express();
-var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// Static directory
-app.use(express.static("app/public"));
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-
-// Routes
-// =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
-var db = require("./app/models");
-
-// Starts the server to begin listening
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
