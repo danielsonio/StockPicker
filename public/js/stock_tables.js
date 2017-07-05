81a540b61255d51e1ade57961f297582c75d14bb
Jordan Gross <jgross18@me.com>
2017-07-04 23:13:11 -0500

    creates the portfolio

--- public/js/stock_tables.js
+++ public/js/stock_tables.js
@@ -1,7 +1,7 @@
  var table = [];
 
  //Database Functions
- //Create the initial database:
+ //Create the initial transaction datatable:
  var transactionTableInit = function() {
      $('#myTable').DataTable({
          responsive: true,
@@ -19,10 +19,33 @@
 
  };
 
+ //Create the initial portfolio datatable:
+ var portfolioTableInit = function() {
+     $('#myPort').DataTable({
+         responsive: true,
+         "searching": false,
+         data: table,
+         columns: [
+             { title: "STOCK NAME" },
+             { title: "QUANTITY" },
+             { title: "PRICE PER STOCK" },
+             { title: "TOTAL STOCK VALUE" },
+             { title: "SELL" },
+         ],
+         "columnDefs": [{
+             "targets": -1,
+             "data": null,
+             "defaultContent": "<button>Sell</button>"
+         }]
+     });
+
+ };
 
 
  transactionTableInit();
+ portfolioTableInit();
 
+ //fetch the transaction table for the user and place it inot the transaction table, destroy allows for a reload button to be created
  var transactionTableLoad = function() {
      var transactionTable = $('#myTable').DataTable();
      transactionTable.destroy();
@@ -37,15 +60,16 @@
              "searching": false,
              data: data,
              columns: [
-                 { data: "createdAt" },
-                 { data: "boughtorsold",
+                 { data: "createdAt" }, {
+                     data: "boughtorsold",
                      render: function(data, type, row) {
-                         if(data){
-                         	return "Sold";
+                         if (data) {
+                             return "Sold";
                          } else {
-                         	return "Bought";
+                             return "Bought";
                          }
-                     } },
+                     }
+                 },
                  { data: "symbol" },
                  { data: "quantity" }, {
                      data: "price",
@@ -54,7 +78,7 @@
                      }
                  }, {
                      "mData": function(data, type, dataToSet) {
-                       return "$" +  data.quantity * data.price;
+                         return "$" + data.quantity * data.price;
                      }
                  },
 
@@ -69,3 +93,78 @@
  };
 
  transactionTableLoad();
+
+
+ //fetch the transaction table for the user, find all the different stocks they have bought and add them into an object
+ //then subtract from it items they have sold, destroy allows for a reload button to be created
+ var portfolioTableLoad = function() {
+     var portfolioTable = $('#myPort').DataTable();
+     portfolioTable.destroy();
+     var portfolio = ["test"];
+
+     $.get("/api/transactions", function(data) {
+         // findAll returns all entries for a table when used with no options
+
+         // We have access to the todos as an argument inside of the callback function
+         console.log(data);
+         console.log(data[0].boughtorsold);
+         
+
+         for (var i = 0; i < data.length; i++) {
+             //go though the response and take all the buy orders, insert them into a portfolio holder, check if a stock of the same name was purchased
+             //add the quantities together if so.
+
+             if (data[i].boughtorsold === false) {
+                 //check if the portfolio already contains a stock with the same name
+                 if (portfolio[0] === "test") {
+                     portfolio.shift();
+                     var myStock = { symbol: data[i].symbol, quantity: data[i].quantity };
+                     portfolio.push(myStock);
+                 } else {
+                     for (var j = 0; j < portfolio.length; j++) {
+                         if (data[i].symbol == portfolio[j].symbol) {
+                             portfolio[j].quantity += data[i].quantity;
+                             break;
+                         }
+                         if (j + 1 == portfolio.length) {
+                             var myStock = { symbol: data[i].symbol, quantity: data[i].quantity };
+                             portfolio.push(myStock);
+                             break;
+                         }
+                     }
+
+                 }
+             } else {
+                 for (var j = 0; j < portfolio.length; j++) {
+                     if (data[i].symbol == portfolio[j].symbol) {
+                         portfolio[j].quantity -= data[i].quantity;
+                         break;
+                     }
+                 }
+             }
+         }
+         console.log(portfolio);
+     }).then(function() {
+         $('#myPort').DataTable({
+             responsive: true,
+             "searching": false,
+             data: portfolio,
+             columns: [
+                 { data: "symbol" },
+                 { data: "quantity" },
+                 { data: "quantity" },
+                 { data: "quantity" },
+                 { title: "SELL" },
+             ],
+             "columnDefs": [{
+                 "targets": -1,
+                 "data": null,
+                 "defaultContent": "<button>Sell</button>"
+             }]
+         });
+     })
+ };
+
+
+
+ portfolioTableLoad();
